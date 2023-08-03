447e77ba41c70fbd404da96f1630045a41d434c8
Yen Mai <yenmai478@gmail.com>
2023-08-03 19:34:55 +0700

    Init project 🎉

---
+++ src/main/java/me/yenmai/simpleapp/SimpleappApplication.java
@@ -0,0 +1,13 @@
+package me.yenmai.simpleapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/yenmai/simpleapp/SimpleappApplicationTests.java
@@ -0,0 +1,13 @@
+package me.yenmai.simpleapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
