ccd20904d688e2a0459f5877f3dae49d094c6ab3
Yen Mai <yenmai478@gmail.com>
2023-08-03 23:22:31 +0700

    Update UT

--- src/test/java/me/yenmai/simpleapp/SimpleappApplicationTests.java
+++ src/test/java/me/yenmai/simpleapp/SimpleappApplicationTests.java
@@ -3,7 +3,6 @@ package me.yenmai.simpleapp;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class SimpleappApplicationTests {
 
 	@Test
