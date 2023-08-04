2fbf19d34294eb3b743768a52de24f080f4c76b9
Yen Mai <43849128+yenmai477@users.noreply.github.com>
2023-08-04 22:26:02 +0700

    Feature/dockerize application (#1)
    * Handle dockerize and init ci cd
    
    * Create deploy.yml
    
    * Remove redandunt code
    
    * Update UT
    
    * Update main.yml
    
    * Update main.yml
    
    * Update main.yml
    
    * Update main.yml
    
    * Add sonar properties
    
    * Update main.yml
    
    * Update main.yml
    
    * Update main.yml
    
    * Add jacoco reporty
    
    * Update main.yml
    
    * Update main.yml
    
    * Update main.yml
    
    * Update main.yml
    
    * Update main.yml
    
    * Update main.yml
    
    * Add prod docker compose
    
    * Update main.yml
    
    * Update main.yml
    
    * Update main.yml
    
    * Update main.yml
    
    * Update main.yml
    
    * Update main.yml
    
    * Update main.yml
    
    * Update main.yml
    
    * Update main.yml
    
    * Update main.yml

---
+++ src/main/java/me/yenmai/simpleapp/controller/ServerInfo.java
@@ -0,0 +1,14 @@
+package me.yenmai.simpleapp.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("server-info")
+public class ServerInfo {
+  @RequestMapping("health-check")
+  ResponseEntity<String> healthCheck() {
+    return ResponseEntity.ok("Response from server!");
+  }
+}
