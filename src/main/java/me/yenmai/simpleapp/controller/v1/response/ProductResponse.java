a097f5f3188ec43a31718b28eba46114d7fd6b58
Yen Mai <43849128+yenmai477@users.noreply.github.com>
2023-08-12 17:07:38 +0700

    Add product api (#3)

---
+++ src/main/java/me/yenmai/simpleapp/controller/v1/response/ProductResponse.java
@@ -0,0 +1,13 @@
+package me.yenmai.simpleapp.controller.v1.response;
+
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ProductResponse {
+  private String name;
+
+}
